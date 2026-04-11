27da8f310ad1e30cfd770762d87b8cf8ddbcc8b9
victordelunafreire-rgb <victordelunafreire@gmail.com>
2026-04-11 01:39:03 -0300

    adicao e configuracao do mongodb

--- src/database/index.js
+++ src/database/index.js
@@ -1,21 +1,30 @@
+import mongoose from 'mongoose';
 import { Sequelize } from 'sequelize';
 import Category from '../app/models/Category.js';
 import Product from '../app/models/Product.js';
 import User from '../app/models/user.js';
 import databaseConfig from '../config/database.cjs';
 
-
 const models = [User, Product, Category];
 
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
     this.connection = new Sequelize(databaseConfig.development);
-    models.map((model) => model.init(this.connection))
-    models.map(model => model.associate && model.associate(this.connection.models));
+    models.map((model) => model.init(this.connection));
+    models.map(
+      (model) => model.associate && model.associate(this.connection.models),
+    );
+  }
+
+  mongo() {
+    this.mongooseConnection = mongoose.connect(
+      'mongodb://localhost:27017/devburguer',
+    );
   }
 }
 
