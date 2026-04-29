023daf5cb95bed8987b0f2a107418ed163bbf089
victordelunafreire-rgb <victordelunafreire@gmail.com>
2026-04-29 00:52:43 -0300

    feat:connecting API with front-end via axios and solving AXIOS issues

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
+import cors from 'cors';
 import express from 'express';
 import fileRouteConfig from './config/filesRoutes.cjs';
 import routes from './routes.js';
 
-
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/product-file', fileRouteConfig);
